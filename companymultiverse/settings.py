e22b0a6aa3f0fbff25f2381cdb5297075f125e58
Sameer <liteshgoyal55@gmail.com>
2026-05-20 17:25:19 +0530

    use database url2

--- companymultiverse/settings.py
+++ companymultiverse/settings.py
@@ -93,9 +93,34 @@ WSGI_APPLICATION = 'companymultiverse.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+
+#     'default': {
+
+#         'ENGINE':
+#         'django.db.backends.postgresql',
+
+#         'NAME':
+#         config('POSTGRES_DB'),
+
+#         'USER':
+#         config('POSTGRES_USER'),
+
+#         'PASSWORD':
+#         config('POSTGRES_PASSWORD'),
+
+#         'HOST':
+#         config('POSTGRES_HOST'),
+
+#         'PORT':
+#         config('POSTGRES_PORT'),
+#     }
+# }
 DATABASES = {
-    "default": dj_database_url.parse(
-        config("DATABASE_URL")
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 # Password validation
