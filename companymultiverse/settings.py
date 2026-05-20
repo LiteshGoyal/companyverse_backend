6d5a4fe247b5d9d0149c3c729179adecc7705731
Sameer <liteshgoyal55@gmail.com>
2026-05-20 17:53:21 +0530

    Database entered correctly

--- companymultiverse/settings.py
+++ companymultiverse/settings.py
@@ -93,36 +93,36 @@ WSGI_APPLICATION = 'companymultiverse.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-
-    'default': {
-
-        'ENGINE':
-        'django.db.backends.postgresql',
-
-        'NAME':
-        config('POSTGRES_DB'),
-
-        'USER':
-        config('POSTGRES_USER'),
-
-        'PASSWORD':
-        config('POSTGRES_PASSWORD'),
-
-        'HOST':
-        config('POSTGRES_HOST'),
-
-        'PORT':
-        config('POSTGRES_PORT'),
-    }
-}
 # DATABASES = {
-#     'default': dj_database_url.config(
-#         default=config('DATABASE_URL'),
-#         conn_max_age=600,
-#         ssl_require=True
-#     )
+
+#     'default': {
+
+#         'ENGINE':
+#         'django.db.backends.postgresql',
+
+#         'NAME':
+#         config('POSTGRES_DB'),
+
+#         'USER':
+#         config('POSTGRES_USER'),
+
+#         'PASSWORD':
+#         config('POSTGRES_PASSWORD'),
+
+#         'HOST':
+#         config('POSTGRES_HOST'),
+
+#         'PORT':
+#         config('POSTGRES_PORT'),
+#     }
 # }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
